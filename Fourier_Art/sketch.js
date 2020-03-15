ae7b188fe17849d9483c215fa42c8bfb0962292e
Md. Zarif Ul Alam <zarif98sjs@gmail.com>
2020-03-15 15:27:27 +0600

    Circle to Wave

--- Fourier_Art/sketch.js
+++ Fourier_Art/sketch.js
@@ -1,4 +1,5 @@
 let time = 0;
+let wave = []; 
 
 function setup()
 {
@@ -8,12 +9,25 @@ function setup()
 function draw()
 {
     background(10,100,0);
-    translate(200,200);
+    translate(400,400);
 
-    let radius = 50;
+    let radius = 150;
     stroke(255);
     noFill();
-    ellipse(0,0,radius*2);
+	ellipse(0,0,radius*2);
+	
+	let x = radius * cos(time);
+	let y = radius * sin(time);
+	wave.push(y);
 
-    time += 0.01;
+	for(let i = 0;i<wave.length;i++)
+	{
+		point(i+radius,wave[i]);
+	}
+
+	fill(255);
+	line(0,0,x,y);
+	ellipse(x,y,10);
+
+    time += 0.1;
 }
